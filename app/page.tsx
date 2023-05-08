dd7acd8d8dc2d19b08428cf1d2c8a964b66f2c7a
Amir Eliz <amir.eliz155@gmail.com>
2023-05-08 03:05:14 +0300

    less opacity

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import MainContainer from './MainContainer'
 const Home = () => {
 	return (
 		<>
-			<div className='absolute left-52 top-4 h-4/5 w-4/5 blur-[120px] opacity-[0.40] bg-gradient-to-r rounded-full from-indigo-500 to-pink-500 -z-10' />
+			<div className='absolute left-52 top-4 h-4/5 w-4/5 blur-[120px] opacity-[0.30] bg-gradient-to-r rounded-full from-indigo-500 to-pink-500 -z-10' />
 			<MainContainer />
 		</>
 	)
